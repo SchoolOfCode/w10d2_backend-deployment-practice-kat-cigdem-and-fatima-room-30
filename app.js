eba1d267515c8ff917d90e914b483d2076b09e70
CigdemOzturk <cigdemozturk_@hotmail.com>
2022-01-25 15:37:25 +0000

    shanges done

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", shoppingListRouter);
+app.use("/items", shoppingListRouter);
 
 export default app;
